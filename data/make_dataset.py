377c457f689b34cdf2cffa3ed6399da13548d5a2
Sudhir Zanje <zanjesud@gmail.com>
2025-07-29 13:54:45 +0530

    Commit all other files which are changed

---
+++ data/make_dataset.py
@@ -0,0 +1,11 @@
+# src/data/make_dataset.py
+import pandas as pd
+from sklearn.datasets import load_iris
+
+def save_raw():
+    iris = load_iris(as_frame=True)
+    df = pd.concat([iris.data, pd.Series(iris.target, name="target")], axis=1)
+    df.to_csv("data/raw/iris.csv", index=False)
+
+if __name__ == "__main__":
+    save_raw()
